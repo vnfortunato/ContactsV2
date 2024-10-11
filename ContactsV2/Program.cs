0f699e02bab8b1af3d070e81e96a0e31d76cf008
vnfortunato <vnfortunato@codearchitects.com>
2024-10-11 09:48:49 +0200

    Added switch and while

--- ContactsV2/Program.cs
+++ ContactsV2/Program.cs
@@ -1,21 +1,38 @@
-﻿string[] array = { "Nome", "Cognome", "Età", "Telefono" };
-string separator = ", ";
-string Array = string.Join(separator, array);
-{
-    bool fileExists = File.Exists(@"C:\Users\Nick\Desktop\File.csv");
+﻿var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop\File.csv";
 
-    using (StreamWriter sw = new StreamWriter(@"C:\Users\Nick\Desktop\File.csv", true))
-    {
-        if (fileExists == false)
-        {
-            sw.WriteLine(Array);
-        }
-        var nome = Console.ReadLine();
-        var cognome = Console.ReadLine();
-        var eta = Console.ReadLine();
-        var telefono = Console.ReadLine();
-        string[] Array2 = { nome, cognome, eta, telefono };
-        string Array3 = string.Join(separator, Array2);
-        sw.WriteLine(Array3);
-    }
+Console.WriteLine("1 - Crea rubrica e aggiungi contatto");
+var scelta = Console.ReadLine();
+while (scelta != "1")
+{
+    scelta = Console.ReadLine();
 }
+    switch (scelta)
+    {
+        case "1":
+            string[] array = { "Nome", "Cognome", "Email", "Telefono" };
+            string separator = ", ";
+            string Array = string.Join(separator, array);
+            {
+                bool fileExists = File.Exists(folderPath);
+
+                using (StreamWriter sw = new StreamWriter(folderPath, true))
+                {
+                    if (fileExists == false)
+                    {
+                        sw.WriteLine(Array);
+                    }
+                    Console.Write("Inserisci nome: ");
+                    var nome = Console.ReadLine();
+                    Console.Write("Inserisci cognome: ");
+                    var cognome = Console.ReadLine();
+                    Console.Write("Inserisci email: ");
+                    var email = Console.ReadLine();
+                    Console.Write("Inserisci telefono: ");
+                    var telefono = Console.ReadLine();
+                    string[] Array2 = { nome, cognome, email, telefono };
+                    string Array3 = string.Join(separator, Array2);
+                    sw.WriteLine(Array3);
+                    break;
+                }
+            }
+    }
